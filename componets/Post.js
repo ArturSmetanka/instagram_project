1301249bf247d74b2db074e009be7073b484c704
ArturSmetanka <smetankaartur@gmail.com>
2021-12-23 15:55:29 +0100

    Add 4 bottoms on bot of posts,caption and comments

--- componets/Post.js
+++ componets/Post.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import { View, Text , StyleSheet, Image} from 'react-native'
 import Divider from 'react-native-divider'
+import { TouchableOpacity } from 'react-native'
+
+const postFooterIcons = [
+    {
+        name: 'Like',
+        imageUrl:
+        'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png',
+        likedImageUrl:'https://img.icons8.com/emoji/96/000000/heart-suit.png'
+    },
+    {
+        name: 'Comment',
+        imageUrl:
+        'https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/ffffff/external-comment-chat-flatart-icons-outline-flatarticons-2.png',
+        
+    },
+    {
+        name: 'Share',
+        imageUrl:
+        'https://img.icons8.com/external-outline-juicy-fish/60/ffffff/external-share-arrows-outline-outline-juicy-fish.png',
+        
+    },
+    {
+        name: 'Save',
+        imageUrl:
+        'https://img.icons8.com/ios/50/ffffff/instagram-new--v1.png',
+        
+    },
+]
 
 
 const Post = ({post}) => {
@@ -10,6 +38,14 @@ const Post = ({post}) => {
            
             <PostHeader post={post} />
             <PostImage post={post} />
+            <View style = {{marginHorizontal: 15, marginTop: 10}}>
+                <PostFooter/>
+                <Likes post ={post}/>
+                <Caption post = {post}/>
+                <CommentsSection post = {post} />
+                <Comments post={post}/>
+            </View>
+            
         </View>
     )
 }
@@ -43,6 +79,63 @@ const PostImage = ({post}) =>(
 
 )
 
+const PostFooter = () => (
+    <View style ={{flexDirection: 'row',justifyContent: 'space-between'}}>
+        <View style = {styles.leftFooterIconsContainer}>
+            <Icon imgStyle={ styles.footerIcon} imgUrl={postFooterIcons[0].imageUrl}/>
+            <Icon imgStyle={ styles.footerIcon} imgUrl={postFooterIcons[1].imageUrl}/>
+            <Icon imgStyle={ styles.footerIcon} imgUrl={postFooterIcons[2].imageUrl}/>
+        </View>
+        <View>
+        <Icon imgStyle={ styles.footerIcon} imgUrl={postFooterIcons[3].imageUrl}/>
+        </View>
+    </View>
+   
+)
+
+const Icon = ({imgStyle,imgUrl}) => (
+    <TouchableOpacity>
+        <Image style = {imgStyle} source={{uri: imgUrl}}/>
+    </TouchableOpacity>
+)
+
+const Likes = ({post}) => (
+    <View style = {{ flexDirection: 'row', marginTop: 4}}>
+     <Text style={{color: 'white',fontWeight: 'bold'}}> {post.likes.toLocaleString('en')}</Text>
+    </View>
+)
+
+const Caption = ({post}) => (
+    <View style = {{marginTop: 5}}> 
+    <Text style = {{color :'white'}}>
+       <Text style = {{fontWeight:'bold'}}>{post.user}</Text> 
+       <Text> {post.caption}</Text> 
+        
+     </Text>
+     </View>
+)
+
+const CommentsSection = ({post}) => (
+    <View style = {{marginTop: 5}}>
+        <Text style ={{color: 'gray'}}>
+            View all comments
+        </Text>
+    </View>
+)
+
+const Comments = ({post}) => (
+    <>
+    {post.commets.map((comment, index) => (
+        <View key = {index} style = {{flexDirection: 'row', marginTop: 5}}>
+            <Text style = {{color: 'white'}}> 
+                <Text style = { { fontWeight: 'bold'}}>{comment.user}</Text>
+                 {' '}{comment.comment}
+            </Text>
+        </View>
+    ))}
+    </>
+)
+
 const styles = StyleSheet.create({
     story:{
         width: 35,
@@ -52,8 +145,14 @@ const styles = StyleSheet.create({
         borderWidth: 1.6,
         borderColor: '#ff8501',
     },
-    names:{
-        color: '#ffffff',
+    footerIcon :{
+        width: 33,
+        height: 33,
+    },
+    leftFooterIconsContainer:{
+        flexDirection: 'row',
+        width: '32%',
+        justifyContent: 'space-between',
     }
 })
 
--- data/post.js
+++ data/post.js
@@ -3,13 +3,13 @@ import { USERS } from './users'
 export const POSTS = [{
     imageUrl: 'https://i.pinimg.com/originals/f7/36/4f/f7364f3a5dca808d53886c450bcb69e2.jpg',
     user: USERS[0].user,
-    likes:10593,
+    likes:'10,593 likes',
     caption: 'Now I show you ...',
     profile_picture: USERS[0].image,
     commets:[
         {
             user: 'baba_jaga123',
-            comment: 'Legend!'
+            comment: 'Vety nice!'
         },
         {
             user: 'redCwhite',
@@ -21,7 +21,7 @@ export const POSTS = [{
 {
     imageUrl: 'https://images.alphacoders.com/121/thumb-1920-121893.jpg',
     user: USERS[1].user,
-    likes:10593,
+    likes:'3,645 likes',
     caption: 'Now I show you ...',
     profile_picture: USERS[1].image,
     commets:[
@@ -40,13 +40,13 @@ export const POSTS = [{
 {
     imageUrl: 'https://lh3.googleusercontent.com/proxy/Bwj5AxgUkPEQ3FvPWH7NFRh7VokXb6zeTQdtxy12grpWh9DQR7FXxgxtnZXddQiuqhCuQmxcp9xJ46FZJusX_xL16Ho0Ac6VymcA3ZZ5MvMQXJLA-nRO1Yta_OCpb4g7',
     user: USERS[2].user,
-    likes:10593,
+    likes:'22,515 likes',
     caption: 'Now I show you ...',
     profile_picture: USERS[2].image,
     commets:[
         {
             user: 'baba_jaga123',
-            comment: 'Legend!'
+            comment: 'Vety nice!'
         },
         {
             user: 'redCwhite',
@@ -59,7 +59,7 @@ export const POSTS = [{
 {
     imageUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b9821bf8-6e6a-4855-ba34-35144c74a205/d8l6neu-50154af3-45ee-4a87-ab2e-8facb5c3b6d0.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I5ODIxYmY4LTZlNmEtNDg1NS1iYTM0LTM1MTQ0Yzc0YTIwNVwvZDhsNm5ldS01MDE1NGFmMy00NWVlLTRhODctYWIyZS04ZmFjYjVjM2I2ZDAuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SCzJVt_fUn82PSQ66LHJSoVi22Bcq89d3-hbryoxiNs',
     user: USERS[3].user,
-    likes:10593,
+    likes:'8,422 likes',
     caption: 'Now I show you ...',
     profile_picture: USERS[3].image,
     commets:[
@@ -78,7 +78,7 @@ export const POSTS = [{
 {
     imageUrl: 'https://wallpaperaccess.com/full/4515141.jpg',
     user: USERS[4].user,
-    likes:10593,
+    likes:'55,634 likes',
     caption: 'Now I show you ...',
     profile_picture: USERS[4].image,
     commets:[
